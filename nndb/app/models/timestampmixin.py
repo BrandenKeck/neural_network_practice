45dbf7e1c7b24e61e0c0721c4356847ee9d949fa
Branden Keck <bkeck@its.jnj.com>
2022-12-14 16:12:09 -0500

    cleanup

--- nndb/app/models/timestampmixin.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from app import db
-
-
-class TimestampMixin:
-    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated = db.Column(db.DateTime, default=datetime.utcnow,
-                        onupdate=datetime.utcnow)
