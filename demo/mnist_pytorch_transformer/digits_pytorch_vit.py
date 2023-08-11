2574150fca5ed437e1dbb22dac21a65b1b2ef02b
Branden Keck <bmkeck62@gmail.com>
2023-08-11 11:29:20 -0400

    cleanup

--- demo/mnist_pytorch_transformer/digits_pytorch_vit.py
+++
@@ -1,8 +0,0 @@
-import torch
-import numpy as np
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.utils.data import TensorDataset, DataLoader
-
-# TODO
